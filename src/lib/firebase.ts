d0c856ac54acb012371d351940e770c261ec6254
Daniel Lauding <daniel@asteria.ai>
2024-12-23 00:48:53 +0100

    Commit message describing your changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwP0w894sxwbxwPwCMR7_i6i8Cowdt2dA",
-  authDomain: "drooly.firebaseapp.com",
-  projectId: "drooly",
-  storageBucket: "drooly.appspot.com",
-  messagingSenderId: "727727122836",
-  appId: "1:727727122836:web:582334a12d884d0ab2c781",
-  measurementId: "G-YKZBQKZ7TK"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
