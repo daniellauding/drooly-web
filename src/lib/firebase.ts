b600748bc9a4c124de3c6bc926e63394a88c4be3
Daniel Lauding <daniel@asteria.ai>
2024-12-28 15:39:09 +0100

    OK version with profile settings etc

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,7 +13,7 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
